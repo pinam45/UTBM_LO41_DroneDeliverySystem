ac4cb2dee56ee78c6da577d57bfcc995bbc2210f
Jérôme BOULMIER <jerome.boulmier@outlook.fr>
2017-05-20 00:11:19 +0200

    Add package struct.

---
+++ DroneDeliverySystem/include/package.h
@@ -0,0 +1,12 @@
+#ifndef UTBM_LO41_DRONEDELIVERYSYSTEM_PACKAGE_HPP
+#define UTBM_LO41_DRONEDELIVERYSYSTEM_PACKAGE_HPP
+
+/**
+ * @brief Represents a package.
+ */
+typedef struct {
+	int priority; //!< priority of a package
+	int weight; //!< weight of a package
+} Package;
+
+#endif //UTBM_LO41_DRONEDELIVERYSYSTEM_PACKAGE_HPP
