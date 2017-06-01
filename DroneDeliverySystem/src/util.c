c5d3770e9c1ae5fe8187dc05b678ed30ab377693
Maxime Pinard <maxime.pin@live.fr>
2017-06-01 21:13:14 +0200

    Fixed check function

--- DroneDeliverySystem/src/util.c
+++ DroneDeliverySystem/src/util.c
@@ -12,7 +12,7 @@ void check(int result, const char* format, ...) {
 		va_start(args, format);
 		vfprintf(stderr, format, args);
 		va_end(args);
-		fprintf(stderr, ": %s/n",strerror(errno));
+		fprintf(stderr, ": %s\n",strerror(errno));
 		exit(1);
 	}
 }
