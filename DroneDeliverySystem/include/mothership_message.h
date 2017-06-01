7e4ae94a7851d418bda5c5e3faec7bd9c1f02b67
Maxime Pinard <maxime.pin@live.fr>
2017-06-01 21:52:26 +0200

    Added DroneMessage and MothershipMessage

---
+++ DroneDeliverySystem/include/mothership_message.h
@@ -0,0 +1,19 @@
+#ifndef UTBM_LO41_DRONEDELIVERYSYSTEM_MOTHERSHIP_MESSAGE_H
+#define UTBM_LO41_DRONEDELIVERYSYSTEM_MOTHERSHIP_MESSAGE_H
+
+#include "typedefs.h"
+
+enum mothership_message_type{
+	DRONE_BACK_TO_MOTHERSHIP,
+	DRONE_DONE_CHARGING,
+	DRONE_PACKAGE_DELIVERED_SUCCESS,
+	DRONE_PACKAGE_DELIVERED_FAIL
+	//TODO
+};
+
+struct mothership_message{
+	MothershipMessageType type;
+	unsigned int sender_id;
+};
+
+#endif //UTBM_LO41_DRONEDELIVERYSYSTEM_MOTHERSHIP_MESSAGE_H
--- DroneDeliverySystem/include/typedefs.h
+++ DroneDeliverySystem/include/typedefs.h
@@ -6,4 +6,10 @@ typedef struct drone Drone;
 typedef struct client Client;
 typedef struct package Package;
 
+typedef enum mothership_message_type MothershipMessageType;
+typedef struct mothership_message MothershipMessage;
+
+typedef enum drone_message_type DroneMessageType;
+typedef struct drone_message DroneMessage;
+
 #endif //UTBM_LO41_DRONEDELIVERYSYSTEM_TYPEDEFS_H
