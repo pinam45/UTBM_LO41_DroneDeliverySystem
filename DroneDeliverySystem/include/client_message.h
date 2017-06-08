b37a91038a50db55ec5a7b5dff82e230b1eb6ed6
Maxime Pinard <maxime.pin@live.fr>
2017-06-08 12:29:35 +0200

    Implemented client behaviour

--- DroneDeliverySystem/include/client_message.h
+++ DroneDeliverySystem/include/client_message.h
@@ -4,7 +4,8 @@
 typedef enum client_message_type{
 	DRONE_PUT_TARGET,
 	DRONE_DELIVERY_SUCCESS,
-	DRONE_DELIVERY_FAILURE // no target
+	DRONE_DELIVERY_FAILURE, // no target, will try again
+	DRONE_DELIVERY_FINAL_FAILURE // no target, no retry
 } ClientMessageType;
 
 struct client_message{
--- DroneDeliverySystem/src/client.c
+++ DroneDeliverySystem/src/client.c
@@ -2,14 +2,18 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <util.h>
-#include <client_message.h>
-#include <log.h>
 #include <pthread.h>
+#include <errno.h>
+#include <time.h>
+#include <mqueue.h>
+
+#include "util.h"
+#include "client_message.h"
+#include "log.h"
 
 static struct mq_attr attr;
 
-static bool process_message(Client* client, ClientMessage* message);
+static void process_message(Client* client, ClientMessage* message);
 
 Client* client_constructor(unsigned int id, unsigned int distance, unsigned int packagesToReceive, unsigned int targetInstalledTime) {
 	Client* client = (Client*) malloc(sizeof(Client));
@@ -28,7 +32,7 @@ Client* client_constructor(unsigned int id, unsigned int distance, unsigned int 
 	attr.mq_msgsize = sizeof(ClientMessage);
 	if((client->msgQueueID = mq_open(buffer, O_RDWR | O_CREAT, 0660, &attr)) == -1) {
 		char errorBuffer[30];
-		sprintf(errorBuffer, "Could not create client %03d", client->id);
+		sprintf(errorBuffer, "Could not create client %03d\n", client->id);
 		perror(errorBuffer);
 
 		free(client);
@@ -48,7 +52,7 @@ Client* client_constructor(unsigned int id, unsigned int distance, unsigned int 
 void client_free(Client* client) {
 	if(mq_close(client->msgQueueID) == -1) {
 		char errorBuffer[30];
-		sprintf(errorBuffer, "Could not destroy client %03d", client->id);
+		sprintf(errorBuffer, "Could not destroy client %03d\n", client->id);
 		perror(errorBuffer);
 	}
 
@@ -57,13 +61,30 @@ void client_free(Client* client) {
 
 void* client_launch(void* client) {
 	Client* client1 = (Client*) client;
-	bool ended = false;
-	while(!ended){
-		ClientMessage clientMessage;
-		check((int) mq_receive(client1->msgQueueID, (char*) &clientMessage, sizeof(ClientMessage), 0),
-		      "mq_receive failed");
-		ended = process_message(client1, &clientMessage);
+	ClientMessage clientMessage;
+	struct timespec time;
+	time.tv_sec = client1->targetInstalledTime;
+	time.tv_nsec = 0;
+	ssize_t result;
+
+	while(client1->packagesToReceive > 0){
+		result = mq_timedreceive(client1->msgQueueID, (char*) &clientMessage, sizeof(ClientMessage), 0, time);
+		if(result < 0){
+			if(errno != ETIMEDOUT){
+				check(result, "mq_receive failed\n");
+			}
+			else{
+				if(client1->targetInstalled){
+					client1->targetInstalled = false;
+					LOG_INFO("Client %03d target removed\n", client1->id);
+				}
+			}
+		}
+		else{
+			process_message(client1, &clientMessage);
+		}
 	}
+	LOG_INFO("Client %03d end of delivery\n", client1->id);
 	pthread_exit(0);
 }
 
@@ -71,22 +92,24 @@ void client_sendMessage(Client* client, ClientMessage* message) {
 	check(mq_send(client->msgQueueID, (const char*) message, sizeof(ClientMessage), 0), "mq_send failed");
 }
 
-bool process_message(Client* client, ClientMessage* message) {
+void process_message(Client* client, ClientMessage* message) {
 	switch(message->type){
 		case DRONE_PUT_TARGET:
 			client->targetInstalled = true;
-			LOG_INFO("Client %03d target installed", client->id);
+			LOG_INFO("Client %03d target installed\n", client->id);
 			break;
 		case DRONE_DELIVERY_SUCCESS:
-			client->targetInstalled = false;
-			LOG_INFO("Client %03d package received", client->id);
-			return true;
+			--(client->packagesToReceive);
+			LOG_INFO("Client %03d package received\n", client->id);
+			break;
 		case DRONE_DELIVERY_FAILURE:
-			client->targetInstalled = false;
-			LOG_INFO("Client %03d package not received", client->id);
+			LOG_INFO("Client %03d package not received\n", client->id);
+			break;
+		case DRONE_DELIVERY_FINAL_FAILURE:
+			--(client->packagesToReceive);
+			LOG_INFO("Client %03d package not received for the last time\n", client->id);
 			break;
 		default:
 			break;
 	}
-	return false;
 }
