3a7bdb75b7edc9e0c3687472f11e9a550aa280f9
Maxime Pinard <maxime.pin@live.fr>
2017-06-09 09:21:53 +0200

    check function update

--- DroneDeliverySystem/src/util.c
+++ DroneDeliverySystem/src/util.c
@@ -3,16 +3,20 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ConsoleControl.h>
 
 #include "util.h"
 
 void check(int result, const char* format, ...) {
-	if (result < 0) {
+	if(result < 0) {
 		va_list args;
 		va_start(args, format);
+		cc_Vector2 pos = {0, 0};
+		cc_setCursorPosition(pos);
+		cc_setColors(RED, WHITE);
 		vfprintf(stderr, format, args);
 		va_end(args);
-		fprintf(stderr, ": %s\n",strerror(errno));
+		fprintf(stderr, ": %s\n", strerror(errno));
 		exit(1);
 	}
 }
